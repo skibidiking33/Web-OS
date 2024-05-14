a2df42194ebe943be66a0c58845c479320ea2c40
skibidiking33 <152452894+skibidiking33@users.noreply.github.com>
2024-05-14 13:14:52 -0700

    Update script.js

--- login/script.js
+++ login/script.js
@@ -1,5 +1,11 @@
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
 
 
 fucntion submitPressed() {
-  
+  if (username == Carter){
+    if (password == Hemsley){
+      window.location.href = "../home/index.html";
+
+    }
 }
